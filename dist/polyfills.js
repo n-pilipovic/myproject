df92f9ee3f15e6a51155cd1d6cc51ab49b7535b5
Novica Pilipovic <novica.pilipovic@fincore.com>
2016-11-09 13:35:09 +0100

    Fixed dependencies

--- dist/polyfills.js
+++ dist/polyfills.js
@@ -88,7 +88,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://n-pilipovic.github.io/bachelor/";
+/******/ 	__webpack_require__.p = "https://n-pilipovic.github.io/myproject/dist/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
